3dbd342eba689156048f76b770c8d40e30d5ad83
Shamandah <shirleenamanda@gmail.com>
2026-07-31 16:17:20 +0300

    Connect frontend to Render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-const api = axios.create({
-    baseURL: "http://127.0.0.1:8000/api/",
+const API = axios.create({
+    baseURL: "https://civic-lens-ai-1.onrender.com/api/",
 });
-
 // Automatically attach JWT token
 api.interceptors.request.use(
     (config) => {
